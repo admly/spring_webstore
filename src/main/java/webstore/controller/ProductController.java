d4b726ba9fc954ec3e8298157cf837cb46e30fdc
admly <admly@interia.pl>
2016-11-26 16:27:17 +0100

    Added DAO and repository with products

--- src/main/java/webstore/controller/ProductController.java
+++ src/main/java/webstore/controller/ProductController.java
@@ -1,20 +1,21 @@
 package webstore.controller;
-import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import webstore.domain.Product;
+
+import webstore.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping("/products")
 	public String list(Model model) {
-		Product iphone = new Product("P1234", "IPhone 5s", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone 5s, smartfon z 4-calowym wyświetlaczem o rozdzielczości 640X1136 oraz 8-megapikselowym aparatem");
-		iphone.setCategory("Smart Phone");
-		iphone.setManufacturer("Apple");
-		iphone.setUnitsInStock(1000);
-		model.addAttribute("product", iphone);
+		model.addAttribute("products", productRepository.getAllProducts());
 		return "products";
-	}
+		}
 }
--- src/main/java/webstore/domain/Product.java
+++ src/main/java/webstore/domain/Product.java
@@ -1,16 +1,17 @@
 package webstore.domain;
 import java.math.BigDecimal;
 public class Product {
-private String productId;
-private String name;
-private BigDecimal unitPrice;
-private String description;
-private String manufacturer;
-private String category;
-private long unitsInStock;
-private long unitsInOrder;
-private boolean discontinued;
-private String condition;
+	private String productId;
+	private String name;
+	private BigDecimal unitPrice;
+	private String description;
+	private String manufacturer;
+	private String category;
+	private long unitsInStock;
+	private long unitsInOrder;
+	private boolean discontinued;
+	private String condition;
+	
 	public Product() {
 		super();
 		}
